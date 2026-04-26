b6fccc7a04f454cb0736615d055edf76f44bc94b
Gandhi <gandhirahmawan@gmail.com>
2026-04-26 21:40:48 +0700

    feat: add Freeware page UI

---
+++ resources/views/freeware.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.app')
+
+@section('title', 'Freeware')
+
+@section('content')
+    <div class="main-freeware">
+        {{-- Top bar --}}
+        <div style="display: flex; align-items: center; gap: 10px;">
+            <button class="sidebar-button">
+                <img src="{{ asset('menu.svg') }}" height="30" width="30" alt="Menu" />
+            </button>
+            <h1>Freeware</h1>
+            <div style="width: 100%; display: flex; flex-direction: column; align-items: flex-end;">
+                <button class="login-button">Login</button>
+            </div>
+        </div>
+
+        {{-- New Products --}}
+        <div class="list-sortir">
+            <button class="sort-button">
+                <img src="{{ asset('sort.svg') }}" height="30" width="40" alt="Filter" />
+            </button>
+            <div class="sortir">
+                <button class="sort">Price</button>
+                <button class="sort">Name</button>
+                <button class="sort">Rating</button>
+                <button class="ascend-descend-button">
+                    <img class="arrow-sort" src="{{ asset('asc-dsc.svg') }}" height="25" width="25" alt="Ascending" />
+                    Ascending</button>
+            </div>
+        </div>
+
+        <div class="list-product-freeware">
+            {{--
+            Replace this static loop with a dynamic @foreach when
+            you pass $products from your controller, e.g.:
+            Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+            @foreach ($products as $product)
+            <div class="product"> ... </div>
+            @endforeach
+            --}}
+
+            @for ($i = 0; $i < 8; $i++) {{-- tergantung banyaknya data di database --}}
+                <div class="product">
+                    <div class="thumbnail-product">
+                        <p style="color: black;">Ini Thumbnail Produk</p>
+                    </div>
+                    <p class="nama-produk">Nama Produk</p>
+                    <p class="deskripsi-singkat-produk">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor (Maksimal 120 karakter spasi juga ikut)
+                    </p>
+                </div>
+            @endfor
+        </div>
+    </div>
+
+@endsection
